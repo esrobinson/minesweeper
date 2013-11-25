863b01b42024fda5a8083192ddff2d4aba4b6988
App Academy Student <student@appacademy.io>
2013-11-25 13:36:16 -0800

    Filled in Tile class and did something with board

--- board.rb
+++ board.rb
@@ -1,8 +1,15 @@
 class Board
 
   def initialize
-
+    @tiles = Array.new(9) { Array.new(9) { Tile.new() } }
   end
+
+  def make_move(move)
+    move_type, coords = move
+    move_type == "f" ? flag(coords) : reveal(coords)
+  end
+
+
 
   def won?
     false
--- tile.rb
+++ tile.rb
@@ -1,3 +1,39 @@
 class Tile
 
+  def initialize
+    @bomb = false
+    @flagged = false
+    @revealed = false
+    @neighbor_bombs = 0
+
+  end
+
+  def bomb?
+    @bomb
+  end
+
+  def flagged?
+    @flagged
+  end
+
+  def revealed?
+    @revealed
+  end
+
+  def flag
+    @flagged = !@flagged
+  end
+
+  def reveal
+    @revealed = true
+    return @neighbor_bombs unless bomb?
+    return "Boom!"
+  end
+
+  def to_s
+    return "* " unless @flagged || @revealed
+    return "f " if @flagged
+    return "#{@neighbor_bombs} "
+  end
+
 end
