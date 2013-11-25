c3d34bff21299f0f65a02d3040713e6b9c107f88
App Academy Student <student@appacademy.io>
2013-11-25 15:44:44 -0800

    Adds file save/load

--- minesweeper.rb
+++ minesweeper.rb
@@ -1,5 +1,6 @@
 require_relative 'board'
 require_relative 'tile'
+require 'yaml'
 
 class Game
 
@@ -10,7 +11,6 @@ class Game
   end
 
   def play
-    @board = Board.new
     lost = false
     until @board.won? || lost
       move = get_move
@@ -18,17 +18,41 @@ class Game
     end
     puts "You win" if @board.won?
     puts "You lose" if lost
+  end
 
+  def start
+    puts "Choose (n)ew game or (l)oad game? "
+    game_type = gets.chomp.downcase[0]
+    if game_type == "n"
+      @board = Board.new
+    else
+      puts "Please enter file name: "
+      file_name = gets.chomp
+      @board = YAML.load(File.read(file_name))
+    end
+    play
   end
 
   def get_move
     @board.print
-    puts "Choose flag or reveal: "
-    move_type = gets.chomp
-    puts "Choose a square: "
-    coords = gets.chomp
-    move = coords.split(",").map(&:to_i)
+    puts "Choose (f)lag, (r)eveal or (s)ave: "
+    move_type = gets.chomp.downcase[0]
+    if move_type == "s"
+      puts "Please enter File Name: "
+      file_name = gets.chomp
+      save_game(file_name)
+      get_move
+    end
+      puts "Choose a square: "
+      coords = gets.chomp
+      move = coords.split(",").map(&:to_i)
     [move_type, move]
+  end
+
+  def save_game(filename)
+    File.open(filename, "w") do |f|
+      f.puts @board.to_yaml
+    end
   end
 
 
