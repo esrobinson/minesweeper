b172e0b4fa0de026b7d7d78fd06f34caf64665ea
App Academy Student <student@appacademy.io>
2013-11-25 14:03:20 -0800

    Added board#reveal_adjacen_tiles

--- board.rb
+++ board.rb
@@ -23,6 +23,19 @@ class Board
     @tile_grid[x][y]
   end
 
+  def reveal_adjacent_tiles(coords)
+    coords_x, coords_y = coords
+    neighbor_offsets =
+        [[-1,1],[0,1],[1,1],[1,0],[1,-1],[0,-1],[-1,-1],[-1,0]]
+    [].tap do |neighbors|
+      neighbor_offsets.each do |offset_x,offset_y|
+        next unless (coords_x + offset_x).between?(0,8)
+        next unless (coords_y + offset_y).between?(0,8)
+        neighbors << @tile_grid[coords_x + offset_x][coords_y + offset_y]
+      end
+
+  end
+
   def won?
     false
     true if #(all mines flagged && no of mines == no of flags) || all others              revealed
