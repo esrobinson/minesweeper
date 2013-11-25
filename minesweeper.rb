361e3613799cbc8ecf1d612d34fe1d823fee0b97
App Academy Student <student@appacademy.io>
2013-11-25 13:17:17 -0800

    Adds get_move and fills in play a bit

--- minesweeper.rb
+++ minesweeper.rb
@@ -8,7 +8,20 @@ class Game
     @board = Board.new
 
     until @board.won? || @board.lost?
+      move = get_move
+      @board.make_move(move)
+    end
 
+  end
+
+  def get_move
+    @board.print
+    puts "Choose flag or reveal: "
+    move_type = gets.chomp
+    puts "Choose a square: "
+    coords = gets.chomp
+    move = coords.split(",").map (&:to_i)
+    [move_type, move]
   end
 
 
